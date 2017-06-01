855c23966189ad5286f97cc10c7c95f204b1989b
chenzhangli01 <chenzhangli01@baidu.com>
2017-06-01 19:43:48 +0800

    2 hours learing for SpringBoot
    Change-Id: I2549b45554ccd8613efd2dd073d74443d209dc31

---
+++ src/main/java/com/immoc/GirlRepository.java
@@ -0,0 +1,14 @@
+package com.immoc;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Created by chenzhangli01 on 2017/6/1.
+ */
+public interface GirlRepository extends JpaRepository<Girl, Integer>{  //这是 jpa 中对应的操作数据库的接口，和 mybatis 的 mapper 作用一样
+
+    // 通过年龄来查询 ， jpa 自带的只有根据 id 来查询, 名字一定要按照规定的格式，这样jpa 才能自动生成函数
+    public List<Girl>  findByAge(Integer age);
+}
